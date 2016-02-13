0f9b673b2ead0eee5c2b95c772ab5b28a2be493a
CubaHub <Hubacub@lmaoayy.ml>
2016-02-13 13:42:37 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -3,6 +3,7 @@ print("welcome to le name generator where we generate v cheap gud names 4 u ok")
 from random import choice
 len = int(input("how long of a nick do u want ok"))
 name = 'r'
+print('ok that is v good nick i like v cheap ok')
 for i in range(len):
      name += choice ('abcdefg')+('esh')+choice ('12345')
 print(name)
