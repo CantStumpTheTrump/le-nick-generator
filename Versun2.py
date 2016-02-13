2943337f50b6fbf0861d836d85d6defff64cbe11
CubaHub <Hubacub@lmaoayy.ml>
2016-02-13 01:57:24 -0500

    Update and rename V2.py to Versun2.py

--- Versun2.py
+++ Versun2.py
@@ -1,5 +1,5 @@
 print("welcome to le name generator where we generate v cheap gud names 4 u ok")
 from random import choice
 name = ''
-name += choice ('abcdefghijklmnopqrstuvwxyz')+choice('aj')+choice('12345678910')+('esh')
+name += choice ('abcdefghijklmnopqrstuvwxyz')+choice('aj')+('esh')+choice('102030405060708090')
 print(name)
