d40e4d818cb0c1d27af4462234a7cd762954b9b7
CubaHub <Hubacub@lmaoayy.ml>
2016-02-13 18:23:47 -0500

    8===D
    Removed the 52 from your mom.

--- Versun2.py
+++ Versun2.py
@@ -1,5 +1,5 @@
 print("welcome to le name generator where we generate v cheap gud names 4 u ok")
 from random import choice
 name = ''
-name += choice ('bcdfghjklmnpqrstvwxyz')+('aj')+('esh')+('52')
+name += choice ('bcdfghjklmnpqrstvwxyz')+('aj')+('esh')
 print(name)
