38487a52e46f9f74b4e5048cbe1883db3d3a5f15
CubaHub <Hubacub@lmaoayy.ml>
2016-02-13 02:00:28 -0500

    Update Versun2.py

--- Versun2.py
+++ Versun2.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3
 print("welcome to le name generator where we generate v cheap gud names 4 u ok")
 from random import choice
 name = ''
-name += choice ('abcdefghijklmnopqrstuvwxyz')+choice('aj')+('esh')+choice('102030405060708090')
+name += choice ('abcdefghijklmnopqrstuvwxyz')+choice('aj')+('esh')+('52')
 print(name)
