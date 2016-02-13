43847cdcdadc3bfcf1f1e0c990495cd6e7616482
CubaHub <Hubacub@lmaoayy.ml>
2016-02-13 01:53:28 -0500

    Update V2.py

--- V2.py
+++ V2.py
@@ -1,5 +1,5 @@
 print("welcome to le name generator where we generate v cheap gud names 4 u ok")
 from random import choice
-name = 'r'
+name = ''
 name += choice ('abcdefghijklmnopqrstuvwxyz')+choice('aj')+choice('12345678910')+('esh')
 print(name)
