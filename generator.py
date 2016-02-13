fe59156c07a34fbfb309c8c747ab75528f36c7bf
CubaHub <Hubacub@lmaoayy.ml>
2016-02-12 23:11:53 -0500

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
-import random
+from random import choice
 len = 10
 name = 'r'
 for i in range(len):
-     name += random.choice ('abcdefghijklmnopqrstuvwxyz')+('esh')+random.choice ('12345678910111213')
+     name += choice ('abcdefghijklmnopqrstuvwxyz')+('esh')+choice ('12345678910111213')
 print(name)
